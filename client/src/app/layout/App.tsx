169831440ce34b02e0742b18a1e11be3ed83d03f
Akib Anwar <akibanwar20@gmail.com>
2023-08-20 12:22:48 -0400

    End of Section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,15 +1,16 @@
-import Catalog from "../../features/catalog/Catalog";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Header from "./Header";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   const [darkMode, setDarkMode] = useState(false);
   const palletType = darkMode ? 'dark' : 'light';
 
-  const theme  = createTheme({
+  const theme = createTheme({
     palette: {
       mode: palletType,
       background: {
@@ -24,6 +25,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
