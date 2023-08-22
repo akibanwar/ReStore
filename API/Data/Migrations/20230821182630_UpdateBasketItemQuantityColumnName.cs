99cd0f44f4240df7dd847b7000f7d4f9a168a43a
Akib Anwar <akibanwar20@gmail.com>
2023-08-22 13:23:37 -0400

    End of Section 6

---
+++ API/Data/Migrations/20230821182630_UpdateBasketItemQuantityColumnName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBasketItemQuantityColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Qunantity",
+                table: "BasketItems",
+                newName: "Quantity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Quantity",
+                table: "BasketItems",
+                newName: "Qunantity");
+        }
+    }
+}
