a79035ab9dacf7f8ad5d7de733831f956d7025a6
Akib Anwar <akibanwar20@gmail.com>
2023-08-27 17:19:10 -0400

    Inventory complete

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -9,7 +9,7 @@ export default function BasketPage() {
     const subtotal = basket?.items.reduce((sum, item) => sum + (item.price * item.quantity), 0) ?? 0;
     const deliveryFee = subtotal > 10000 ? 0 : 500;
 
-    if (!basket)
+    if (!basket || basket.items.length === 0)
         return <Typography variant="h3">Your basket is empty.</Typography>
 
     return (
